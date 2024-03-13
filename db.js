ab8815a81c9ff86b1184f58b79ac8c5c39f1cdce
unknown <ayushsingh.8d@gmail.com>
2024-03-13 11:41:44 +0530

    fix

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 //change the password to access the local mySql database;
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'ayush',
+  host: 'localhost',
+  user: 'root',
+  password: 'ayush',
 });
 export default db;
